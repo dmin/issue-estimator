2ceb37510d7daedb8bd3881ca2b0fb3464746a42
David Mininger <dmininger+github@gmail.com>
2020-10-22 10:43:59 -0500

    Fetch reckon-api issues.

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  console.log("Hello");
-  core.setOutput("time", "This is the time output.");
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const token = core.getInput("token");
+    const user = core.getInput("user");
+    const octokit = github.getOctokit(token);
+
+    // TODO: Support pagination of issues / projects
+    // TODO: Combine queries?
+
+    const reckonAPI = await octokit.graphql(
+      `
+        query Issues($user: String!) {
+          repository(name: "reckon-api", owner: $user) {
+            name
+            issues(first:100, states: OPEN) {
+              edges {
+                node {
+                  number
+                  title
+                  body
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        user,
+      }
+    );
+
+    console.log("Issues in Reckon API:");
+    reckonAPI.repository.issues.edges.forEach((edge) => {
+      console.log(edge.node.title);
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
